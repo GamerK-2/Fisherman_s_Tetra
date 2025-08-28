66d70e92a778f39b9ede43301e8660e88b0f6f58
GamerK-2 <a01054678017@gmail.com>
2025-08-28 22:45:21 +0900

    Initial Uploading Fully Functional Fishing Rod

--- src/main/java/net/gamerk2/fisherman_s_tetra/item/ModularFishingRodItem.java
+++ src/main/java/net/gamerk2/fisherman_s_tetra/item/ModularFishingRodItem.java
@@ -1,6 +1,8 @@
 package net.gamerk2.fisherman_s_tetra.item;
 
+import com.google.common.collect.ImmutableList;
 import net.gamerk2.fisherman_s_tetra.FishermanConfigHandler;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
@@ -11,19 +13,27 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.FishingHook;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.registries.ObjectHolder;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import se.mickelus.mutil.network.PacketHandler;
+import se.mickelus.tetra.TetraMod;
 import se.mickelus.tetra.data.DataManager;
 import se.mickelus.tetra.items.modular.IModularItem;
 import se.mickelus.tetra.items.modular.ItemModularHandheld;
+import se.mickelus.tetra.module.ItemModule;
 import se.mickelus.tetra.module.SchematicRegistry;
+import se.mickelus.tetra.module.data.ModuleModel;
 import se.mickelus.tetra.module.schematic.RepairSchematic;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @ParametersAreNonnullByDefault
 public class ModularFishingRodItem extends ItemModularHandheld {
@@ -32,6 +42,9 @@ public class ModularFishingRodItem extends ItemModularHandheld {
     public static final String reelkey = "fishingrod/reel";
 
     public static final String identifier = "modular_fishingrod";
+
+    // fishing line resource location
+    protected ModuleModel fishingLineModel0 = new ModuleModel("static", new ResourceLocation(TetraMod.MOD_ID, "item/module/fishingrod/line_0"));
 
     @ObjectHolder(registryName = "item", value = "tetra:modular_fishingrod")
     public static ModularFishingRodItem instance;
@@ -54,8 +67,6 @@ public class ModularFishingRodItem extends ItemModularHandheld {
         this.honeIntegrityMultiplier = honeIntergrityMultiplier;
     }
 
-
-
     @Override
     public void commonInit(PacketHandler packetHandler) {
         DataManager.instance.synergyData.onReload(() -> synergies = DataManager.instance.synergyData.getOrdered("fishingrod/"));
@@ -64,8 +75,8 @@ public class ModularFishingRodItem extends ItemModularHandheld {
     public static ItemStack setupFishingRod() {
         ItemStack itemStack = new ItemStack(instance);
 
-        IModularItem.putModuleInSlot(itemStack, rodkey, "fishingrod/rod", "fishingrod/rod_material", "rod/stick");
-        IModularItem.putModuleInSlot(itemStack, reelkey, "fishingrod/reel", "fishingrod/reel_material", "reel/oak");
+        IModularItem.putModuleInSlot(itemStack, rodkey, "fishingrod/basic_rod", "fishingrod/basic_rod_material", "basic_rod/oak");
+        IModularItem.putModuleInSlot(itemStack, reelkey, "fishingrod/basic_reel", "fishingrod/basic_reel_material", "basic_reel/oak");
 
         IModularItem.updateIdentifier(itemStack);
         return itemStack;
@@ -77,11 +88,12 @@ public class ModularFishingRodItem extends ItemModularHandheld {
         Level world = pPlayer.level();
         if (pPlayer.fishing != null) {
             if (!pLevel.isClientSide) {
-                if (!world.isClientSide) {
-                    this.applyDamage(1, itemstack, pPlayer);
-                }
                 int i = pPlayer.fishing.retrieve(itemstack);
-                // if the hooked object was a fish, this will tick the honing progression
+                // damage the fishing rod by 1 durability when reeling in something (even if nothing was caught)
+                if (!world.isClientSide) {
+                    this.applyDamage(i, itemstack, pPlayer);
+                }
+                // if the hooked object was an item, this will tick the honing progression
                 if (itemstack.getItem() instanceof IModularItem && i == 1) {
                     tickHoningProgression(pPlayer, itemstack, 2);
                 }
@@ -93,9 +105,9 @@ public class ModularFishingRodItem extends ItemModularHandheld {
         } else {
             pLevel.playSound(null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.FISHING_BOBBER_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
             if (!pLevel.isClientSide) {
-                int k = EnchantmentHelper.getFishingSpeedBonus(itemstack);
-                int j = EnchantmentHelper.getFishingLuckBonus(itemstack);
-                pLevel.addFreshEntity(new FishingHook(pPlayer, pLevel, j, k));
+                int luck = this.getEnchantmentLevel(itemstack, Enchantments.FISHING_LUCK);
+                int speed = this.getEnchantmentLevel(itemstack, Enchantments.FISHING_SPEED);
+                pLevel.addFreshEntity(new FishingHook(pPlayer, pLevel, luck, speed));
             }
 
             pPlayer.awardStat(Stats.ITEM_USED.get(this));
@@ -108,5 +120,41 @@ public class ModularFishingRodItem extends ItemModularHandheld {
     @Override
     public boolean canPerformAction(ItemStack stack, net.minecraftforge.common.ToolAction toolAction) {
         return net.minecraftforge.common.ToolActions.DEFAULT_FISHING_ROD_ACTIONS.contains(toolAction);
+    }
+
+
+    private String getCastVariant(@Nullable LivingEntity entity, ItemStack itemStack) {
+        if (entity instanceof Player player) {
+            if (player.fishing != null && itemStack.getItem() instanceof ModularFishingRodItem && player.getMainHandItem() == itemStack) {
+                return "cast";
+            }
+        }
+        return "uncast";
+    }
+
+    @Override
+    public String getModelCacheKey(ItemStack itemStack, LivingEntity entity) {
+        return super.getModelCacheKey(itemStack, entity) + ":" + getCastVariant(entity, itemStack);
+    }
+
+    @Override
+    public ImmutableList<ModuleModel> getModels(ItemStack itemStack, @Nullable LivingEntity entity) {
+        String modelType = getCastVariant(entity, itemStack);
+
+        ImmutableList<ModuleModel> models = getAllModules(itemStack).stream()
+                .sorted(Comparator.comparing(ItemModule::getRenderLayer))
+                .flatMap(itemModule -> Arrays.stream(itemModule.getModels(itemStack)))
+                .filter(Objects::nonNull)
+                .sorted(Comparator.comparing(ModuleModel::getRenderLayer))
+                .filter(model -> model.type.equals(modelType) || model.type.equals("static"))
+                .collect(Collectors.collectingAndThen(Collectors.toList(), ImmutableList::copyOf));
+
+        if (modelType.equals("uncast")) {
+            return ImmutableList.<ModuleModel>builder()
+                    .addAll(models)
+                    .add(fishingLineModel0)
+                    .build();
+        }
+        return models;
     }
 }
